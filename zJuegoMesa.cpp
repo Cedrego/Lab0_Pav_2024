621a8c0889a89b515b30b0c32e3a1c787bfe6ed6
lucaso111 <lucasmozo111@gmail.com>
2024-04-03 22:34:05 -0300

    a

---
+++ zJuegoMesa.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+#include "JuegoMesa.h"
+#include <iostream>
+JuegoMesa::JuegoMesa(){};
+JuegoMesa::JuegoMesa(std::string Nom,int AnioComprado,ESTADO Estado,int CantJugadores,int EdadRecomendada){
+    this->Nombre=Nom;
+    this->AnioComprado=AnioComprado;
+    this->Estado=Estado;
+    this->CantJugadores=CantJugadores;
+    this->EdadRecomendada=EdadRecomendada;
+};
+JuegoMesa::~JuegoMesa(){};
+std::string toString(){
+    return "JuegoMesa: " + this->Nombre + ", "+ (this->AnioComprado)+ ", " +(this->Estado)+ ", "+ this->EdadRecomendada +", "+(this->CantJugadores);
+};
