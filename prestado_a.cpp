7e408dd1f824ea2147ba1cbc96fdd98633a532a3
lucaso111 <lucasmozo111@gmail.com>
2024-04-04 13:25:50 -0300

    a

--- prestado_a.cpp
+++ prestado_a.cpp
@@ -1,7 +1,8 @@
 #include "Objeto.h"
 #include "Niño.h"
 #include <string>
-
+bool verdadero(false);
+string respuesta;
 using namespace prestado;
 Niño::Niño(std::string Nom2, int Edad, std::string Direccion,std::string Telefono){
     this-Nombre=Nom2;
@@ -9,22 +10,30 @@ Niño::Niño(std::string Nom2, int Edad, std::string Direccion,std::string Telef
     this->Direccion=Direccion;
     this->Telefono=Telefono;
 };
- /*no me acuerdo cual era xD "¿ Eres el Niño: " + this->Nombre + ", "+ (this->Edad)+ ", " +(this->Direccion)+ ", "+ (this->Telefono) ?";
- en caso de no ser, pregunta si quiere arreglar la informacion o salir
- si es va a poner el objeto*/
- //bool verdadero(true){
+cout <<"¿ Eres el Niño: "<< Nom2<< " - "<<Edad<<" - "<< Direccion <<" - " << Telefono <</n <<"   ¿ Y - N ?";
+cin >> respuesta;
+if(respuesta=='N'){
+    return "Niño no encontrado";
+}else{if(respuesta=='Y'){
+    verdadero=true;
+}else{return "Error"; }
+};
+ if(verdadero=true){
 Objeto::Objeto(std::string Nom,int AnioComprado,ESTADO Estado){
     this->Nombre=Nom;
     this->AnioComprado=AnioComprado;
     this->Estado=Estado;
+    };
 };
- // "¿Quieres el Objeto: " + this->Nombre + ", "+ (this->AnioComprado)+ ", " +(this->Estado) ?");
+ cout << "¿Quieres el Objeto: " << Nom <<" - "<< AnioComprado <<" - " << Estado << /n <<"   ¿ Si - No ?" ;
+    cin >> respuesta;
+if(respuesta=='Y'){
+//une el objeto al niño
+return "Disfrute de su prestamo";
 };
-/*otro bool aca por si no lo quiere o si lo quiere
- bool verdadero(false) o (true);
-}
-para bool true
-une el objeto al niño
-return "disfrute su prestamo";
-para bool false
-return "Prestamo cancelado"*/
+else{
+    if(respuesta=='N'){
+return "Prestamo Cancelado";
+};
+else{ return "Error"};
+};
