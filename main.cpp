ad33b6bedea219af7fd0786aa5e8156a053491ef
Thiago Cedrés <104656328+Cedrego@users.noreply.github.com>
2024-04-04 15:43:09 -0300

    a

--- main.cpp
+++ main.cpp
@@ -4,16 +4,36 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     /*
-todo en un loop quisas un switch o asi pueda funcar
-texto menu de biblioteca
-añadir Niño
-Eliminar Niño
-añadir Objeto
-Eliminar Objeto
-Prestar Objeto
-Listar Objetos Prestados
-Listar Objetos Rotos
+    char op;
+    do{
+        cout<<"[texto menu de biblioteca]"<<"\n";
+        cout<<"añadir Niño"<<"\n";
+        cout<<"Eliminar Niño"<<"\n";
+        cout<<"añadir Objeto"<<"\n";
+        cout<<"Eliminar Objeto"<<"\n";
+        cout<<"Prestar Objeto"<<"\n";
+        cout<<"Listar Objetos Prestados"<<"\n";
+        cout<<"Listar Objetos Rotos"<<"\n";
+        cout<<"Finalizar programa"<<"\n";
 
+        switch(op){
+            case '1':
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
+                break;
+            case '6':
+                break;
+            case '7':
+        }
+    }while(op!='8');
+
+todo en un loop quisas un switch o asi pueda funcar
 loop al 
 añadir Niño
 Añadir Objeto
