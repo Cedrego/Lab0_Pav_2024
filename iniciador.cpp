0f21f9c6faaa7e4e88e1ff20d266c747aa748159
lucaso111 <lucasmozo111@gmail.com>
2024-04-10 16:57:56 -0300

    a

---
+++ iniciador.cpp
@@ -0,0 +1,33 @@
+/*
+set<Niño*> conjuntoN; //conjunto de Niños
+set<Objeto*> conjuntoO; //conjunto de Objetos
+
+
+Libro *libroNuevo = new Libro("Nacidos de la bruma: El imperio final",stoi(2022),Roto,688, "Brandon Sanderson");
+Libro *libroNuevo = new Libro("Las Malas",stoi(2022), Nuevo, 240, "Camila Sosa Villada");
+Libro *libroNuevo = new Libro("El cocodrilo al que no le gustaba el agua",stoi(2016),Roto,32,"Gemma Merino");
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+juegos de mesa
+JuegoMesa *juegoMesaNuevo = new JuegoMesa("Juego Uno",stoi(2022), Roto, 10,7);
+JuegoMesa *juegoMesaNuevo = new JuegoMesa("Mazo de Cartas",stoi(2019), Nuevo, 4,7);
+JuegoMesa *juegoMesaNuevo = new JuegoMesa("Dados",stoi(2020), Roto, 6,2);
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+kids(dolor de huevos)
+
+conjuntoN.insert(new Niño("María Laura",stoi(10),"Nueva Palmira 1521",099298190));
+conjuntoN.insert(new Niño("Alex",stoi(5),"Humberto Primo 1501",29094141));
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+prestamos
+
+⚫ Niño = María Laura Objeto = Mazo de Cartas
+⚫ Niño = María Laura Objeto = Nacidos de la bruma: El imperio final
+⚫ Niño = María Laura Objeto = Dados
+⚫ Niño = Alex Objeto = Juego Uno
+⚫ Niño = Alex Objeto = El cocodrilo al que no le gustaba el agua
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+
