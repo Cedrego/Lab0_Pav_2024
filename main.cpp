63f027b1bba02352f3ad957b2c2f4b7d711b9617
Thiago Cedrés <104656328+Cedrego@users.noreply.github.com>
2024-04-10 20:27:41 -0300

    k

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char *argv[]){
                 cout<<"Indique el nombre del niño que desea borrar: ";
                 cin>>dedChaild;
                 
-                for (Niño *ninio: conjuntoN){
+                for (Niño *ninio : conjuntoN){
                     if (ninio-> getNombre()==dedChaild){
                         nEncontrado=ninio;
                     }
